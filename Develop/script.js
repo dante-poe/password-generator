49e23701d3eaa0419da6b1b9cbac56fa5ea0791b
Sage-Poe <dante.s.poe@gmail.com>
2021-04-14 15:41:04 -0700

    arrays and math random

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var generatePassword ='
+// Arrays
+var numArray = [0,1,2,3,4,5,6,7,8,9];
+var lowArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specArray = ['!','@','#','$','%','&','?',]; 
+// randomize arrays
+var randomLower = lowArray[Math.floor(Math.random() * lowArray.length)];
+var randomUpper = upArray[Math.floor(Math.random() * upArray.length)];
+var randomNumber = numArray[Math.floor(Math.random() * numArray.length)];
+var randomSpecial = specArray[Math.floor(Math.random() * specArray.length)];
+//function for pulling random characters and numbers
+
+function generatePassword(){
+  return randomUpper + randomLower + randomSpecial + randomNumber
+}
+
+// User Prompts
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,6 +28,5 @@ function writePassword() {
 
 }
 
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generate.addEventListener("click", writePassword)
