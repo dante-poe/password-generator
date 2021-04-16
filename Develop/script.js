c8842143e0f51e6c2a13f66cdd3db7c7d862e043
Sage-Poe <dante.s.poe@gmail.com>
2021-04-15 19:56:44 -0700

    still working on storing proper values from prompt

--- Develop/script.js
+++ Develop/script.js
@@ -2,39 +2,35 @@
 var numArray = [0,1,2,3,4,5,6,7,8,9];
 var lowArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var specArray = ['!','@','#','$','%','&','?',]; 
+var specArray = ['!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}','|','`','~','.',',',"'"]; 
 
 // randomize arrays
-var randomLower = lowArray[Math.floor(Math.random() * lowArray.length)];
-var randomUpper = upArray[Math.floor(Math.random() * upArray.length)];
-var randomNumber = numArray[Math.floor(Math.random() * numArray.length)];
-var randomSpecial = specArray[Math.floor(Math.random() * specArray.length)];
+// var randomLower = lowArray[Math.floor(Math.random() * lowArray.length)];
+// var randomUpper = upArray[Math.floor(Math.random() * upArray.length)];
+// var randomNumber = numArray[Math.floor(Math.random() * numArray.length)];
+// var randomSpecial = specArray[Math.floor(Math.random() * specArray.length)];
 
 
-function generatePassword(){
-  
-  //prompts
-  var passLength = prompt ('How long should the password be?')
-  passLength = passLength
-  
-  var passCritNum = confirm('Would you like to include numbers to the password?')
-  passCritNum
-  
-  var passCritSpec = confirm('Would you like to include special characters?')
-  passCritSpec
-  
-  var passCritUp = confirm ('Would you like to include uppercase letters?')
-  passCritUp
 
-  if (passLength >= 8 || passLength <= 128){
-    
+function generatePassword(passLength, passLow, passUp, passSpec){
+  var inputLength = prompt('How long should the password be?');
+  var passLength = parseInt(inputLength);
+  if (inputLength >= 8 || inputLength <= 128){
+    console.log(inputLength);
+
+  }
+  else {
+    alert('Password must be between 8 and 128 characters!')    
+
   }
 
 
-  //display// 
+
+  console.log(passLength)
+  
+
   
 }
-
 
 
 // Write password to the #password input
