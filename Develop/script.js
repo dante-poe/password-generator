bae878ce6e8688fd335e2c36cd4981054378f97d
Sage-Poe <dante.s.poe@gmail.com>
2021-04-15 21:18:27 -0700

    added notation, and a cancel option to the first prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Arrays
+// Arrays to pull from to generate a random password
 var numArray = [0,1,2,3,4,5,6,7,8,9];
 var lowArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -11,21 +11,30 @@ var specArray = ['!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-'
 // var randomSpecial = specArray[Math.floor(Math.random() * specArray.length)];
 
 
-
+//the entire function to generate a random password
 function generatePassword(passLength, passLow, passUp, passSpec){
+  //first prompt about length
   var inputLength = prompt('How long should the password be?');
   var passLength = parseInt(inputLength);
+  //check to see if the input matches the criteria
   if (passLength >= 8 && passLength <=128){
+    //if the input criteria is met, then prompt for special characters
     var passSpec = confirm('Would you like to include special characters?')
+    //
     if (passSpec){
       var passUp = confirm('Would you like to include uppercase letters?') 
     }
     else {
-      alert('they said no')
+    
     }
     
     
   }
+  //if the user hits cancel on the first prompt, end the function.
+  else if (inputLength === null){
+    return;
+  }
+  //alert user that the input did not match criteria
   else {
     alert('Password must be between 8 and 128 characters!')    
     writePassword();
