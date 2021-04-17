0780834d3ebfe37f041b12be9bf94ddd8bf0e4f6
Sage-Poe <dante.s.poe@gmail.com>
2021-04-16 17:01:48 -0700

    fixed arrays and added if else functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,24 @@
 // Arrays to pull from to generate a random password
-var numArray = [0,1,2,3,4,5,6,7,8,9];
+var numArray = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var lowArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var specArray = ['!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}','|','`','~','.',',',"'"]; 
+var upArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specArray = ['!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}','|','`','~','.',',',"'",
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
+var allArray = ['!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}',
+'|','`','~','.',',',"'",'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9];
+var usArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','&','?','(',')','"',
+',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}','|','`','~','.',',',"'"];
+var nsArray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','&','?','(',')','"',',','*','+','>','<','-','=','/',';',':','_','^','[',']','{','}','|','`','~','.',',',"'"];
+var unArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9];
 
 
 
-// randomize arrays
-// var randomLower = lowArray[Math.floor(Math.random() * lowArray.length)];
-// var randomUpper = upArray[Math.floor(Math.random() * upArray.length)];
-// var randomNumber = numArray[Math.floor(Math.random() * numArray.length)];
-// var randomSpecial = specArray[Math.floor(Math.random() * specArray.length)];
+
 
 
 //the entire function to generate a random password
@@ -22,30 +30,52 @@ function generatePassword(){
   //first prompt about length
   var inputLength = prompt('Please set the password length');
   var confirmLength = parseInt(inputLength);
-  
+  console.log(confirmLength);
   //check to see if the input matches the criteria
   if (confirmLength >= 8 && confirmLength <=128){
     
     //if the input criteria is met, then prompt for uppercase letters
     var confirmUp = confirm('Would you like to include uppercase letters?');
     
+    //then prompt for numbers
+    var confirmNum = confirm('Would you like to include numbers?');
+
     //then prompt about special characters
     var confirmSpec = confirm('Would you like to include special characters?'); 
     
-    // if user wants both uppercase and special characters
-    if (confirmUp && confirmSpec){
-      //generate a password using every array
+    // if user wants all three criteria
+    if (confirmUp && confirmSpec && confirmNum){
+      for (var i = 0; i < allArray; i++){
+        var num = Math.floor(Math.random(allArray) * confirmLength) + 1;
+        console.log(num);
+      }
     }
-    // if user wants only uppercase and no special characters
-    else if(confirmUp){
+    // if user wants only uppercase and numbers
+    else if(confirmUp && confirmNum){
 
     }
-    // if user only wants special characters and no uppercase
-    else if(confirmSpec){
+    // upper and spec
+    else if(confirmUp && confirmSpec){
 
     }
-    // if user wants to include neither
-    else{
+    // special and numbers
+    else if(confirmSpec && confirmNum){
+
+    }
+    //just upper
+    else if (confirmUp){
+
+    }
+    //just special
+    else if(confirmSpec) {
+
+    }
+    //just numbers
+    else if (confirmNum){
+
+    }
+    //just lowercase
+    else {
 
     }
   }
